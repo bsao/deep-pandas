0be4c173d31fce6f12ddcc70e433d9e7a80bc30e
bsao <robson.junior@vivareal.com>
2016-08-31 19:59:48 -0300

    new files

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-docker run -d -p 8888:8888 -p 4040:4040 -v $PWD/notebook:/home/jovyan/work bsao/pyspark-notebook start-notebook.sh --NotebookApp.base_url=/
+docker run -d -p 8888:8888 -p 4040:4040 -v $PWD/notebook:/home/jovyan/work bsao/scipy-notebook start-notebook.sh --NotebookApp.base_url=/
